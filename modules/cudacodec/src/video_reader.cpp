3c36bf285a5108c74fe70610707ad237063c2c0f
root <root@localhost.localdomain>
2016-01-25 12:59:16 +0800

    fixed typo

--- modules/cudacodec/src/video_reader.cpp
+++ modules/cudacodec/src/video_reader.cpp
@@ -181,7 +181,7 @@ namespace
             // perform post processing on the CUDA surface (performs colors space conversion and post processing)
             // comment this out if we inclue the line of code seen above
             GpuMat frame_gpu;
-            videoDecPostProcessFrame(decodedFrame, frame, videoDecoder_->targetWidth(), videoDecoder_->targetHeight());
+            videoDecPostProcessFrame(decodedFrame, frame_gpu, videoDecoder_->targetWidth(), videoDecoder_->targetHeight());
             Mat frame_cpu(frame_gpu);
             frame_cpu.copyTo(frame);
             // unmap video frame
